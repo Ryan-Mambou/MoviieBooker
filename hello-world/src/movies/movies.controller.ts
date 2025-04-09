908414e3e12c5ac7f931361fa7cc5d71d9214b63
Ryan-Mambou <ryanmambou@gmail.com>
2025-04-09 12:03:56 +0200

    feat: all endpoints for movies

--- hello-world/src/movies/movies.controller.ts
+++ hello-world/src/movies/movies.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Observable } from 'rxjs';
-import { MovieResponse } from './types';
+import { Genre, Movie, MovieResponse } from './types';
 
 @ApiTags('Movies')
-@Controller('movies')
+@Controller('/')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
-  @Get()
+  @Get('/movies')
   @ApiOperation({ summary: 'Get all movies' })
   @ApiResponse({ status: 200, description: 'All movies' })
   getMovies(
@@ -17,5 +17,33 @@ export class MoviesController {
     @Query('sort_by') sortBy: string,
   ): Observable<MovieResponse> {
     return this.moviesService.getMovies(page, sortBy);
+  }
+
+  @Get('movie/now_playing')
+  @ApiOperation({ summary: 'Get movies playing now' })
+  @ApiResponse({ status: 200, description: 'Movies playing now' })
+  getNowPlayingMovies(): Observable<MovieResponse> {
+    return this.moviesService.getNowPlayingMovies();
+  }
+
+  @Get('/search/movie')
+  @ApiOperation({ summary: 'Search for a movie' })
+  @ApiResponse({ status: 200, description: 'Movie found' })
+  searchMovie(@Query('query') movieName: string): Observable<MovieResponse> {
+    return this.moviesService.searchMovie(movieName);
+  }
+
+  @Get('/movie/:movie_id')
+  @ApiOperation({ summary: 'Get a movie by id' })
+  @ApiResponse({ status: 200, description: 'Movie found' })
+  getMovieById(@Param('movie_id') movieId: string): Observable<Movie> {
+    return this.moviesService.getMovieById(movieId);
+  }
+
+  @Get('/genre/movie/list')
+  @ApiOperation({ summary: 'Get all genres' })
+  @ApiResponse({ status: 200, description: 'All genres' })
+  getGenres(): Observable<Genre[]> {
+    return this.moviesService.getGenres();
   }
 }
--- hello-world/src/movies/movies.service.ts
+++ hello-world/src/movies/movies.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { AxiosResponse } from 'axios';
 import { map, Observable } from 'rxjs';
-import { MovieResponse } from './types';
+import { Genre, Movie, MovieResponse } from './types';
 
 @Injectable()
 export class MoviesService {
@@ -21,5 +21,48 @@ export class MoviesService {
         },
       })
       .pipe(map((response: AxiosResponse<MovieResponse>) => response.data));
+  }
+
+  getNowPlayingMovies(): Observable<MovieResponse> {
+    return this.httpService
+      .get<MovieResponse>(`${this.baseUrl}/movie/now_playing`, {
+        headers: {
+          Authorization: `Bearer ${process.env.MOVIE_BEARER_TOKEN}`,
+        },
+      })
+      .pipe(map((response: AxiosResponse<MovieResponse>) => response.data));
+  }
+
+  searchMovie(movieName: string): Observable<MovieResponse> {
+    return this.httpService
+      .get<MovieResponse>(`${this.baseUrl}/search/movie`, {
+        headers: {
+          Authorization: `Bearer ${process.env.MOVIE_BEARER_TOKEN}`,
+        },
+        params: {
+          query: movieName,
+        },
+      })
+      .pipe(map((response: AxiosResponse<MovieResponse>) => response.data));
+  }
+
+  getMovieById(movieId: string): Observable<Movie> {
+    return this.httpService
+      .get<Movie>(`${this.baseUrl}/movie/${movieId}`, {
+        headers: {
+          Authorization: `Bearer ${process.env.MOVIE_BEARER_TOKEN}`,
+        },
+      })
+      .pipe(map((response: AxiosResponse<Movie>) => response.data));
+  }
+
+  getGenres(): Observable<Genre[]> {
+    return this.httpService
+      .get<Genre[]>(`${this.baseUrl}/genre/movie/list`, {
+        headers: {
+          Authorization: `Bearer ${process.env.MOVIE_BEARER_TOKEN}`,
+        },
+      })
+      .pipe(map((response: AxiosResponse<Genre[]>) => response.data));
   }
 }
